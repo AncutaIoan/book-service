5e042cc68dbc31ee8bc7e7dcce05ce09844d3249
AncutaIoan <eoancuta@gmail.com>
2023-05-13 23:03:37 +0300

    feat: added comments for books

--- src/main/kotlin/learn/bookservice/repodto/BookEntity.kt
+++ src/main/kotlin/learn/bookservice/repodto/BookEntity.kt
@@ -6,15 +6,15 @@ import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("book")
-data class BookEntity (
-    @Id
-    var id: Int? =null,
-    @Column("self_link")
-    var selfLink: String? = null,
-    @Column("sale_info")
-    var saleInfo: String? = null,
-    @Column("volume_info")
-    var volumeInfo: String? = null
+data class BookEntity(
+        @Id
+        var id: Int? = null,
+        @Column("self_link")
+        var selfLink: String? = null,
+        @Column("sale_info")
+        var saleInfo: String? = null,
+        @Column("volume_info")
+        var volumeInfo: String? = null
 ) {
     companion object {
         fun from(item: Item) = BookEntity(selfLink = item.selfLink, saleInfo = "item.saleInfo.toString()", volumeInfo = "item.volumeInfo.toString()")
---
+++ src/main/kotlin/learn/bookservice/repodto/CommentEntity.kt
@@ -0,0 +1,22 @@
+package learn.bookservice.repodto
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
+
+@Table("comment")
+data class CommentEntity(
+        @Id
+        var id: Int? = null,
+        @Column("text")
+        val text: String,
+        @Column("rating")
+        val rating: Int,
+        @Column("added_on")
+        val addedOn: LocalDateTime,
+        @Column("user_id")
+        val userId: String,
+        @Column("book_id")
+        val bookId: String
+)
